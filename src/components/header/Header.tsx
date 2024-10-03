3409619554badd26c1386551c008cd773ddb5207
SilviuNecsulescu <73882256+SilviuNecsulescu@users.noreply.github.com>
2024-10-03 18:23:55 +0300

    updating logo import
    updating logo import

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import logo from "@/assets/Logo.png";
+import logo from "../../assets/Logo.png";
 import Image from "next/image";
 import HeaderNavigation from "../headerNavigation/HeaderNavigation";
 import Search from "../search/Search";
